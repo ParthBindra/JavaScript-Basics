4d108c2910d20be987af17c78257c2efe54144fe
ParthBindra <136303280+ParthBindra@users.noreply.github.com>
2024-05-15 21:39:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("hello world", 4 + 6, "another log");
+console.warn("these is warning");
+console.error("this is an error");
+
+//javascript variable
+//container to store data values
+
+    var number1 = 34;
+    var number2 = 56;
+    console.log(number1 + number2);
+
+    //datatypes in javascript
+    var str1="this is string";
+    var str2= 'this is also string';
+    var num1=455;
+    var num2=24.3;
+    
+
+
+
